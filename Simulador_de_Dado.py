e16fc9d54ba608802aee77c49ae9cfce28a5522d
Jefferson Santos <98091558+oficialjeffs@users.noreply.github.com>
2022-01-29 18:30:01 -0300

    Add files via upload

---
+++ Simulador_de_Dado.py
@@ -0,0 +1,21 @@
+import random
+x=1
+e=input('Deseja jogar o dado?\n-----------------------------\nDigite [s] para sim ou [n] para não.\n-----------------------------\n:')
+
+while x==1:
+    
+    if e=='s':  
+        n=random.randint(1,6)
+        print('O valor é:',n)
+        e=input('\n-----------------------------\nDeseja jogar novamente?\n-----------------------------\nDigite [s] para sim ou [n] para não.\n-----------------------------\n:')
+    elif e=='s':  
+        n=random.randint(1,6)
+        print('O valor é:',n)
+        r=input('\n-----------------------------\nDeseja jogar novamente?\n-----------------------------\nDigite [s] para sim ou [n] para não.\n-----------------------------\n:')
+    
+    elif e=='n':   
+        print('Finalizando Programa')
+        x=0
+    else:
+        print('Digite Apenas valores Válidos')
+        e=input('-----------------------------\nDeseja jogar o dado?\n-----------------------------\nDigite [s] para sim ou [n] para não.\n-----------------------------\n:')
